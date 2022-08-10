1dd18b62c7a3bba763fcef212620303d4f8c407b
miro83kolev <miro83kolev@gmail.com>
2022-08-10 17:23:33 +0300

    update of project - exam preps

--- first_steps_in_oop/rhombus.py
+++ first_steps_in_oop/rhombus.py
@@ -1,32 +1,25 @@
-def get_line(i, n):
-    spaces_count = n - 1 - i
-    stars_count = i + 1
-    return ' ' * spaces_count + ('* ' * stars_count).strip()
+def print_row(row, n):
+    for space in range(n - row):
+        print(" ", end="")
+    for row in range(1, row):
+        print("*", end=" ")
+    print("*")
 
 
-def get_rhombus(n):
-    return [get_line(i, n) for i in range(n)] + \
-        [get_line(i, n) for i in range(n - 2, -1, -1)]
-
-def print_line(n):
-    print(get_line(n - 1, n - 1))
-
-def print_rhombus(n):
-    [print(row) for row in get_rhombus(n)]
+def print_top(n):
+    for row in range(1, n + 1):
+        print_row(row, n)
 
 
-print_rhombus(4)
+def print_bottom(n):
+    for row in range(n - 1, 0, -1):
+        print_row(row, n)
 
 
-def print_rhombus(n):
-    for i in range(n):
-        spaces_count = n - 1 - i
-        stars_count = i + 1
-        print(' ' * spaces_count + '* ' * stars_count)
-    for i in range(n - 2, - 1, -1):
-        spaces_count = n - 1 - i
-        stars_count = i + 1
-        print(' ' * spaces_count + '* ' * stars_count)
+def print_rombus(n):
+    print_top(n)
+    print_bottom(n)
 
-print_rhombus(3)
-print_rhombus(4)
+
+n = int(input())
+print_rombus(n)
