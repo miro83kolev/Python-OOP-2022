c28a45afa121818298952c365570e5dc351840f6
miro83kolev <miro83kolev@gmail.com>
2022-08-21 15:51:35 +0300

    update

--- retake_exam_prep/library_test_remake/project/test/test.py
+++ retake_exam_prep/library_test_remake/project/test/test.py
@@ -51,21 +51,49 @@ class LibraryTest(TestCase):
         self.assertEqual(1, len(self.library.readers))
         self.assertDictEqual({'Peter': []}, self.library.readers)
 
+    def test_add_book_returns_correctly_str(self):
+        self.library.readers['John'] = []
+        result = self.library.add_reader("John")
+        self.assertEqual("John is already registered in the Test library.", result)
 
+    def test_rent_book_when_reader_does_not_exist(self):
+        result = self.library.rent_book('John', 'A', 'book')
+        expected = 'John is not registered in the Test Library.'
+        self.assertEqual(expected, result)
+        self.assertTrue('John' not in self.library.readers)
 
+    def test_rent_book_when_author_does_not_exist(self):
+        self.library.readers['John'] = []
+        result = self.library.rent_book('John', 'A', 'book')
+        expected = "Test Library does not have any A's books."
+        self.assertEqual(expected, result)
+        self.assertDictEqual({}, self.library.books_by_authors)
+        self.assertTrue('A' not in self.library.books_by_authors)
 
+    def test_when_title_does_not_exists(self):
+        self.library.add_reader('John')
+        self.library.books_by_authors['A'] = 'Me'
+        result = self.library.rent_book('John', 'A', 'new')
+        expected = """Test Library does not have A's "new"."""
+        self.assertEqual(expected, result)
+        self.assertTrue('New' not in self.library.books_by_authors['A'])
 
+    def test_rent_a_book_adds_to_readers_list(self):
+        self.library.add_reader('John')
+        self.library.add_book('A', 'book')
+        self.library.rent_book("John", "A", "book")
+        self.assertEqual({'John': [{'A': 'book'}]}, self.library.readers)
+        self.assertTrue('John' in self.library.readers)
+        self.assertTrue('A' in self.library.books_by_authors)
 
-
-
-
-
-
-
-
-
-
-
+    def test_rent_book_removes_title_from_authors_books_list(self):
+        self.library.add_reader('John')
+        self.library.add_book("A", "book 1")
+        self.library.add_book('A', 'book2')
+        self.assertEqual(2, len(self.library.books_by_authors['A']))
+        self.library.rent_book('John', 'A', 'book2')
+        self.assertEqual(1, len(self.library.books_by_authors['A']))
+        self.assertTrue('book2' not in self.library.books_by_authors['A'])
 
 
 if __name__ == '__main__':
